6045bc5ac04fb467241977be548ea53076f982d3
adriano-Peretti <80107070+adriano-Peretti@users.noreply.github.com>
2023-08-07 21:41:38 -0300

    Update ExemploInteger.java

--- 2ºPeriodo/OrientecaoAObjeto_01/aula_02/ExemploInteger.java
+++ 2ºPeriodo/OrientecaoAObjeto_01/aula_02/ExemploInteger.java
@@ -1,22 +1,20 @@
 public class ExemploInteger {
     public static void main(String[] args){
         String s1 = "10";
-        System.out.println("s1 vale" + s1);
+        System.out.println("s1 vale " + s1);
+
         //converter de String para int
         int i1 = Integer.parseInt(s1);
-        System.out.println("i1 vale" + i1);
+        System.out.println("i1 vale " + i1);
 
+        // de int para String
         Integer i2 = 2;
-        s1 = i2.toString(); // de int para String
+        s1 = i2.toString();
         String s2 = Integer.toString(i1);
         System.out.println("s2 vale: " + s2);
 
         //converter para float
         float f1 = i2.floatValue();
         System.out.println("f1 vale: " + f1);
-
-
-
-
     }
 }
